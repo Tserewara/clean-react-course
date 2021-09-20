166bdab563524d58c312a7ea4765dc4b2c81002e
Tserewara <alvaro.alf2@gmail.com>
2021-09-20 11:20:01 -0300

    test: ensure SignUp calls AddAccount with correct values

---
+++ src/presentation/test/mock-add-account.ts
@@ -0,0 +1,13 @@
+import { AccountModel } from '@/domain/models'
+import { AddAccount, AddAccountParams } from '@/domain/usecases'
+import { mockAccountModel } from '@/domain/test'
+
+export class AddAccountSpy implements AddAccount {
+  account = mockAccountModel()
+  params: AddAccountParams
+
+  async add (params: AddAccountParams): Promise<AccountModel> {
+    this.params = params
+    return this.account
+  }
+}
