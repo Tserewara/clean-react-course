6891bb68e4431bf1adcfcda1cdbe4eb827959a12
Tserewara <alvaro.alf2@gmail.com>
2021-10-13 11:39:37 -0300

    test: ensure Input receives focus when label is clicked

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -22,7 +22,8 @@ const Input: React.FC<Props> = (props: Props) => {
         data-testid={props.name}
         readOnly
         onFocus={e => { e.target.readOnly = false }}
-        onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })} />
+        onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })}
+      />
       <label
         data-testid={`${props.name}-label`}
         onClick={() => { inputRef.current.focus() }}
