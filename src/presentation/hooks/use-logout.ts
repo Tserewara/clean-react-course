6f0109cd367c9912fa7ee6af27ce06f83eb08cb6
Tserewara <alvaro.alf2@gmail.com>
2021-11-11 22:15:22 -0300

    refactor: add useLogout to remove code duplication

---
+++ src/presentation/hooks/use-logout.ts
@@ -0,0 +1,14 @@
+import { ApiContext } from '@/presentation/contexts'
+import { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+
+type ResultType = () => void
+
+export const useLogout = (): ResultType => {
+  const history = useHistory()
+  const { setCurrentAccount } = useContext(ApiContext)
+  return (): void => {
+    setCurrentAccount(undefined)
+    history.replace('/login')
+  }
+}
