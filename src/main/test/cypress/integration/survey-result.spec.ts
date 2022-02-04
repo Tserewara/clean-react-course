6961ce965001b0ca5151356009f19288a3d2cec9
Tserewara <alvaro.alf2@gmail.com>
2022-02-04 09:56:36 -0300

    test: ensure SurveyResult logs out if request returns 403

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -4,6 +4,7 @@ import * as Http from '../utils/http-mocks'
 const path = /surveys/
 const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
 const mockSuccess = (): void => Http.mockOk(path, 'GET', 'fx:survey-result')
+const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'GET')
 
 describe('SurveyResult', () => {
   beforeEach(() => {
@@ -25,5 +26,11 @@ describe('SurveyResult', () => {
     mockSuccess()
     cy.getByTestId('reload').click()
     cy.getByTestId('question').should('exist')
+  })
+
+  it('Should log out on AccessDeniedError', () => {
+    mockAccessDeniedError()
+    cy.visit('/surveys/any_id')
+    Helper.testUrl('/login')
   })
 })
