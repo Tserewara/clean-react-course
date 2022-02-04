e7b2bd299f732ce819fb89796c75d11de15f36fa
Tserewara <alvaro.alf2@gmail.com>
2022-02-04 14:32:16 -0300

    feat: ensure RemoteSaveSurveyResult calls HttpClient with correct values

---
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@/data/protocols/http'
+import { RemoteSurveyResultModel } from '@/data/models'
+import { LoadSurveyResult, SaveSurveyResult } from '@/domain/usecases'
+
+export class RemoteSaveSurveyResult implements SaveSurveyResult {
+  constructor (
+    private readonly url: string,
+    private readonly httpClient: HttpClient<LoadSurveyResult.Model>
+  ) {}
+
+  async save (params: SaveSurveyResult.Params): Promise<SaveSurveyResult.Model> {
+    await this.httpClient.request({
+      url: this.url,
+      method: 'put'
+    })
+    return null
+  }
+}
+
+export namespace RemoteSaveSurveyResult {
+  export type Model = RemoteSurveyResultModel
+}
