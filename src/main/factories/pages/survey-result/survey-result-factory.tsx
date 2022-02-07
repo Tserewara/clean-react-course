e23e1bb14f0f81ca068e26965245fff017663654
Tserewara <alvaro.alf2@gmail.com>
2022-02-07 09:31:52 -0300

    feat: ensure SurveyResult calls SaveSurveyResult on non-active answer click

--- src/main/factories/pages/survey-result/survey-result-factory.tsx
+++ src/main/factories/pages/survey-result/survey-result-factory.tsx
@@ -1,5 +1,5 @@
 import { SurveyResult } from '@/presentation/pages'
-import { makeRemoteLoadSurveyResult } from '@/main/factories/usecases'
+import { makeRemoteLoadSurveyResult, makeRemoteSaveSurveyResult } from '@/main/factories/usecases'
 import { useParams } from 'react-router-dom'
 import React from 'react'
 
@@ -11,6 +11,7 @@ export const makeSurveyResult: React.FC = () => {
   return (
     <SurveyResult
       loadSurveyResult={makeRemoteLoadSurveyResult(id)}
+      saveSurveyResult={makeRemoteSaveSurveyResult(id)}
     />
   )
 }
--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,15 +1,16 @@
 import Styles from './survey-result-styles.scss'
 import { Error, Footer, Header, Loading } from '@/presentation/components'
 import { useErrorHandler } from '@/presentation/hooks'
-import { LoadSurveyResult } from '@/domain/usecases'
+import { LoadSurveyResult, SaveSurveyResult } from '@/domain/usecases'
 import React, { useEffect, useState } from 'react'
-import { SurveyResultData } from '@/presentation/pages/survey-result/components'
+import { SurveyResultContext, SurveyResultData } from '@/presentation/pages/survey-result/components'
 
 type Props = {
   loadSurveyResult: LoadSurveyResult
+  saveSurveyResult: SaveSurveyResult
 }
 
-const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
+const SurveyResult: React.FC<Props> = ({ loadSurveyResult, saveSurveyResult }: Props) => {
   const handleError = useErrorHandler((error: Error) => {
     setState(old => ({ ...old, surveyResult: null, isLoading: false, error: error.message }))
   })
@@ -19,21 +20,29 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
     surveyResult: null as LoadSurveyResult.Model,
     reload: false
   })
-
+  const onAnswer = (answer: string): void => {
+    setState(old => ({ ...old, isLoading: true }))
+    saveSurveyResult.save({ answer })
+      .then()
+      .catch()
+  }
+  const reload = (): void => setState(old => ({ isLoading: false, surveyResult: null, error: '', reload: !old.reload }))
   useEffect(() => {
     loadSurveyResult.load()
       .then(surveyResult => setState(old => ({ ...old, surveyResult })))
       .catch(handleError)
   }, [state.reload])
-  const reload = (): void => setState(old => ({ isLoading: false, surveyResult: null, error: '', reload: !old.reload }))
+
   return (
     <div className={Styles.surveyResultWrap}>
       <Header />
-      <div data-testid="survey-result" className={Styles.contentWrap}>
-        {state.surveyResult && <SurveyResultData surveyResult={state.surveyResult}/> }
-        {state.isLoading && <Loading />}
-        {state.error && <Error error={state.error} reload={reload} />}
-      </div>
+      <SurveyResultContext.Provider value={{ onAnswer }}>
+        <div data-testid="survey-result" className={Styles.contentWrap}>
+          {state.surveyResult && <SurveyResultData surveyResult={state.surveyResult} />}
+          {state.isLoading && <Loading />}
+          {state.error && <Error error={state.error} reload={reload} />}
+        </div>
+      </SurveyResultContext.Provider>
       <Footer />
     </div>
   )
